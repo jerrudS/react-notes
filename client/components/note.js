901c824e14118537148af1790af1e9979f84c121
Jerrud Sabourin <jsabourin1235@gmail.com>
2017-08-01 15:50:51 -0700

    Handle form submissions by sending the new note data to the Express app. #3

--- client/components/note.js
+++ client/components/note.js
@@ -3,18 +3,23 @@ import React from 'react'
 export default class Note extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { note: [] }
     this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   handleSubmit(event) {
-    const { value } = event.target
+    event.preventDefault()
+    console.log(event.target)
+    const formData = new FormData(event.target)
+    const userData = {
+      note: formData.get('note')
+    }
+    console.log(userData)
     fetch('/notes', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify(value)
+      body: JSON.stringify(userData)
     })
     .catch(err => {
       console.log('ERROR', err)
@@ -23,17 +28,19 @@ export default class Note extends React.Component {
 
   render() {
     return (
-      <div className="form-inline">
-        <input
-          className="form-control"
-          autoFocus
-          onSubmit={this.handleSubmit}
-          placeholder="Enter Note Here"
-          type="text"/>
-        <button className="btn btn-default" type="submit">
-          Add Note
-        </button>
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <div className="form-inline">
+          <input
+            className="form-control"
+            autoFocus
+            placeholder="Enter Note Here"
+            name="note"
+            type="text"/>
+          <button className="btn btn-default" type="submit">
+            Add Note
+          </button>
+        </div>
+      </form>
     )
   }
 }
