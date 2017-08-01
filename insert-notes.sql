4d1fea05f07e09da535e619e7b71614ab809a8f7
Jerrud Sabourin <jsabourin1235@gmail.com>
2017-08-01 13:54:29 -0700

    Styled page. #1

--- insert-notes.sql
+++ insert-notes.sql
@@ -2,8 +2,8 @@ TRUNCATE TABLE notes;
 
 INSERT INTO notes (note)
 VALUES
-  ('-React is a more efficient way to serve UI components'),
-  ('-Library: you call its code'),
-  ('-Framework: calls your code'),
-  ('-Redux manages state in large applications'),
-  ('-A direct dependency you require into your code')
+  ('React is a more efficient way to serve UI components'),
+  ('Library: you call its code'),
+  ('Framework: calls your code'),
+  ('Redux manages state in large applications'),
+  ('A direct dependency you require into your code')
