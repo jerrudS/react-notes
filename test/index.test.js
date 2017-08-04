8cba46c71a35ae39d93ed606aa05c167c96277b1
Jerrud Sabourin <jsabourin1235@gmail.com>
2017-08-04 13:05:00 -0700

    Adding POST, DELETE. #7

--- test/index.test.js
+++ test/index.test.js
@@ -3,15 +3,38 @@ const { expect } = require('chai')
 const request = require('request')
 
 
-describe('My Notes', () => {
-  it('Should be a JSON array', (done) => {
+describe('GET My Notes', () => {
+  it('Returns an array of notes', (done) => {
     request('http://localhost:3005/notes', (error, response, body) => {
-      console.log('error;', error)
-      console.log('statusCode:', response && response.statusCode)
-      console.log('body:', body)
+      expect(error).to.equal(null)
+      expect(response.statusCode).to.equal(200)
       const data = JSON.parse(body)
       expect(data).to.be.an('array')
       done()
     })
   })
 })
+
+describe('POST My Notes', () => {
+  it('Saves a new note', (done) => {
+    request.post('http://localhost:3005/notes', (error, response, body) => {
+      expect(error).to.equal(null)
+      expect(response.statusCode).to.equal(201)
+      const data = JSON.parse(body)
+      expect(data).to.be.an('array')
+      done()
+    })
+  })
+})
+
+// describe('DELETE My Note', () => {
+//   it('Deletes a note by id', (done) => {
+//     request.post('http://localhost:3005/notes', (error, response, body) => {
+//       console.log(body)
+//       if (err) return done(err)
+//       console.log(body)
+//       expect(body).not.to.equal(undefined)
+//       done()
+//     })
+//   })
+// })
